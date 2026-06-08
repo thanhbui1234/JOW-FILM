4b750c172166c71b7c88f39e6006b90520a13b3c
thanhbui1234 <miktenthanh@gmail.com>
2026-06-09 00:51:37 +0700

    fix ui

--- apps/user-next-app/src/app/page.tsx
+++ apps/user-next-app/src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
       <AboutSection />
       <WeddingHighlightSection />
       <WeddingReelsSection />
-      <TraditionalFilmSection />
+      {/* <TraditionalFilmSection /> */}
       <ContactPreview />
     </main>
   );
